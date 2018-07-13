605e31cc82876351b1bb8e1189eee8b22c235297
Haya4Taka <takahiro@hayashitakahironoMacBook-ea.local>
2018-07-13 19:00:42 +0900

    Delete_Unnesecarry_commentout

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,6 @@ class MessagesController < ApplicationController
         format.json
       end
 
-      # redirect_to group_messages_path(@group), notice: "メッセージが送信されました"
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください'
