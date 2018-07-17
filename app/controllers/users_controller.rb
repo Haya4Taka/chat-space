65a8ece0d356a5dcb8c606d699f95b98217372cf
Haya4Taka <takahiro@hayashitakahironoMacBook-ea.local>
2018-07-17 15:41:26 +0900

    Disabling_searchFunc_when_nothing_input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    if params[:keyword] == ""
+      @users = []
+    else
+      @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    end
+
     respond_to do |format|
       format.html
       format.json
