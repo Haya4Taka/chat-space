974f689de9ed7cc3642fccd0213a3ba73867d058
Haya4Taka <takahiro@hayashitakahironoMacBook-ea.local>
2018-07-17 11:59:46 +0900

    Add_incremental_Search

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,67 @@
+$(function() {
+  var search_result = $("#user-search-result");
+  var group_member = $("#chat-group-users");
+
+  function showUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${ user.name }</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.id } data-user-name=${ user.name }>追加</a>
+                </div>`
+    search_result.append(html);
+  }
+
+  function appendUser(user) {
+    var member_count =$(".js-chat-member").length;
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ member_count +1 }'>
+                  <input class="group_members" name='group[user_ids][]' type='hidden' value='${ user.attr('data-user-id') }'>
+                  <p class='chat-group-user__name'>${ user.attr('data-user-name') }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+  group_member.append(html);
+  }
+
+  function deleteUser(user) {
+    group_member.remove(user);
+  }
+
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users){
+      search_result.empty();
+      var members = $("input[name='group[user_ids][]']");
+      members.each(function() {
+        var member_id = $(this).attr('value');
+        users.some(function(v, i){
+          if (v.id==member_id) users.splice(i,1);
+        });
+      });
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          showUser(user);
+        });
+      };
+    })
+    .fail(function(){
+      alert("ユーザー検索に失敗しました")
+    })
+  });
+  $("#user-search-result").on('click', '.user-search-add', function() {
+    var selected_user = $(this)
+    // console.log(this);
+    // console.log(selected_user.attr('data-user-id'));
+    appendUser(selected_user);
+  });
+  $("#chat-group-users").on("click", '.chat-group-user__btn--remove', function() {
+    $(this).parent().remove();
+  })
+  $("#user-search-result").on("click", '.chat-group-user__btn--add', function() {
+    // console.log($(this).parent());
+    $(this).parent().remove();
+  })
+});
