91b73c9001a4377f789dc913a63faf2723a5987e
Haya4Taka <takahiro@hayashitakahironoMacBook-ea.local>
2018-07-17 16:35:19 +0900

    Apply_jsClass_inSteadOf_cssClass

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -5,7 +5,7 @@ $(function() {
   function showUser(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${ user.id } data-user-name=${ user.name }>追加</a>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id=${ user.id } data-user-name=${ user.name }>追加</a>
                 </div>`
     search_result.append(html);
   }
@@ -55,10 +55,10 @@ $(function() {
     var selected_user = $(this)
     appendUser(selected_user);
   });
-  $("#chat-group-users").on("click", '.chat-group-user__btn--remove', function() {
+  $("#chat-group-users").on("click", '.js-remove-btn', function() {
     $(this).parent().remove();
   })
-  $("#user-search-result").on("click", '.chat-group-user__btn--add', function() {
+  $("#user-search-result").on("click", '.js-add-btn', function() {
     $(this).parent().remove();
   })
 });
