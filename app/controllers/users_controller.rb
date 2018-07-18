b3cd1f2584405abbc33db7c10fdc72044404873c
Haya4Taka <takahiro@hayashitakahironoMacBook-ea.local>
2018-07-18 12:07:05 +0900

    Remove_ProcessForNoInput_atController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    if params[:keyword] == ""
-      @users = []
-    else
-      @users = User.where('name LIKE(?)', "%#{params[:keyword]}%" ).where.not(name: current_user.name)
-    end
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%" ).where.not(name: current_user.name)
 
     respond_to do |format|
       format.html
